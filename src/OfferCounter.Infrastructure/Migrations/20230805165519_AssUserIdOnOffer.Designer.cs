121b478b9e19c8472c81feb28f41cd65e4593460
dhielyton <dhielyton@gmail.com>
2023-08-05 14:02:21 -0300

    add migration userId in Offer

--- src/OfferCounter.Infrastructure/Migrations/20230805165519_AssUserIdOnOffer.Designer.cs
+++ src/OfferCounter.Infrastructure/Migrations/20230805165519_AssUserIdOnOffer.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OfferCounter.Infrastructure.Context;
 
@@ -11,9 +12,10 @@ using OfferCounter.Infrastructure.Context;
 namespace OfferCounter.Infrastructure.Migrations
 {
     [DbContext(typeof(OfferCounterContex))]
-    partial class OfferCounterContexModelSnapshot : ModelSnapshot
+    [Migration("20230805165519_AssUserIdOnOffer")]
+    partial class AssUserIdOnOffer
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/OfferCounter.Infrastructure/Migrations/20230805165519_AssUserIdOnOffer.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OfferCounter.Infrastructure.Migrations
+{
+    public partial class AssUserIdOnOffer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CrationDate",
+                table: "Offer",
+                newName: "CreationDate");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Offer",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Offer");
+
+            migrationBuilder.RenameColumn(
+                name: "CreationDate",
+                table: "Offer",
+                newName: "CrationDate");
+        }
+    }
+}
